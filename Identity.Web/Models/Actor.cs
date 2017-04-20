80a4407f0c46acfcdd125829c0a01dc67c3037f9
Brandon Garcia Quezada <brandongq@gmail.com>
2017-04-19 17:51:05 -0700

    Models

---
+++ Identity.Web/Models/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Identity.Web.Models
+{
+    public class Actor
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string path_image { get; set; }
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
